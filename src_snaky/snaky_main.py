0d7c62dfe52100ccd6a2c0201135834093dd8caa
MichaelCretignier <44569244+MichaelCretignier@users.noreply.github.com>
2026-02-19 08:27:24 +0000

    fix RASSINE NEID reading UT time (1.0.5)

--- src_snaky/snaky_main.py
+++ src_snaky/snaky_main.py
@@ -1425,9 +1425,9 @@ def extract_header(files, instru, debug=False, ra=None, dec=None):
         missing_time = (summary['rjd']!=summary['rjd'])
         print(' [INFO] Nb of missing time = %.0f'%(np.sum(missing_time)))
         if np.sum(missing_time)!=0: #search ut in filename
-            if ins=='NEID':
+            if instrument[0:4]=='NEID':
                 uttime = np.array([f[-17:-2] for f in files])
-                uttime = np.array([f"{s[:4]}-{s[4:6]}-{s[6:8]}T{s[9:11]}:{s[11:13]}:{s[13:15]}" for s in uttime])
+                uttime = np.array([f"{s[:4]}-{s[4:6]}-{s[6:8]}T{s[9:11]}:{s[11:13]}:{s[13:15]}.000" for s in uttime])
             else:
                 uttime = np.array([f[-25:-2] for f in files])
             year = np.array([ut[0:4] for ut in uttime])
